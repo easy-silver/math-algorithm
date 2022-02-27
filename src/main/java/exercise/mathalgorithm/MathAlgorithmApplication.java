4927315143d5c1420328654e1967ab2479d495c0
Timo <developer.timo@gmail.com>
2022-02-28 00:01:52 +0900

    Initial commit

---
+++ src/main/java/exercise/mathalgorithm/MathAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package exercise.mathalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exercise/mathalgorithm/MathAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.mathalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
